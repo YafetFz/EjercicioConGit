56ee234ea267804b42eb3c96693492b995064acb
YafetFz <ayafetfz@gmail.com>
2026-02-13 17:37:37 -0600

    Se agrego una linea que muestra en consola algunos comandos usados en este ejemplo de git

--- EjercicioGit/Program.cs
+++ EjercicioGit/Program.cs
@@ -3,3 +3,4 @@
 //Estudios en desarrollo : Ingenieria en sistemas
 Console.WriteLine("Este es un pequeño ejemplo de usar Git y GitHub");
 Console.WriteLine("Este ejemplo se hizo el 13 de febrero del 2026");
+Console.WriteLine("git init, git commit, git push, git status, git branch, git checkout, git log");
