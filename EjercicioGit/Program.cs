0a49ea399a83ff28f0aa7b8ccff9ad4eb3ac293c
YafetFz <ayafetfz@gmail.com>
2026-02-13 17:29:02 -0600

    Se agrego un poco mas de informacion en los comentarios

--- EjercicioGit/Program.cs
+++ EjercicioGit/Program.cs
@@ -1,3 +1,5 @@
 ﻿//YAFET ALESSANDRO FLORES ZAVALA
+//Direccion: Comayagua Comayagua 
+//Estudios en desarrollo : Ingenieria en sistemas
 Console.WriteLine("Este es un pequeño ejemplo de usar Git y GitHub");
 Console.WriteLine("Este ejemplo se hizo el 13 de febrero del 2026");
